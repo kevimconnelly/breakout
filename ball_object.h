ae5edde78e024833a1b349b6ea5c626a5ad37630
Kevin <kevinconnelly1337@gmail.com>
2024-06-09 11:28:44 -0700

    add ball object class

---
+++ ball_object.h
@@ -0,0 +1,22 @@
+#ifndef BALLOBJECT_H
+#define BALLOBJECT_H
+
+#include "game_object.h"
+#include "texture.h"
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+
+class BallObject : public GameObject
+{
+public:
+	float Radius;
+	bool Stuck;
+
+	BallObject();
+	BallObject(glm::vec2 pos, float radius, glm::vec2 velocity, Texture2D sprite);
+
+	glm::vec2 Move(float dt, unsigned int window_width);
+	void Reset(glm::vec2 position, glm::vec2 velocity);
+};
+
+#endif
